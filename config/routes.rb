1b82cf7214b2580a1ea89645c3d359f71cfd8cbc
Alan <alanmak95@gmail.com>
2021-02-04 21:03:11 +0000

    Added logic to log users in and out

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
