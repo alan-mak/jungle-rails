7a69272ba089fd7d1a04ab9d1a5bd0e945d51c40
Alan <alanmak95@gmail.com>
2021-02-09 05:15:23 +0000

    Added user authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,9 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials email, password
+    @user = User.find_by_email(email.strip.downcase)
+    if @user
+      @user.authenticate(password)
+    end
   end
 end
